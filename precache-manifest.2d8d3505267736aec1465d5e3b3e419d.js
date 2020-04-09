4768f9ac7937de49cd74b2e01470aa61236f259c
Waqas Ali <waqas.abbasi@outlook.com>
2020-04-10 01:30:49 +0800

    Updates

---
+++ precache-manifest.2d8d3505267736aec1465d5e3b3e419d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37ab352ca6c9ee137984935a3cb6dd9",
+    "url": "/Parh/index.html"
+  },
+  {
+    "revision": "1f59d8af1e1424e10ce2",
+    "url": "/Parh/static/css/main.33daf26f.chunk.css"
+  },
+  {
+    "revision": "e98e7b5869bbab5e19c9",
+    "url": "/Parh/static/js/2.feaeb6cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Parh/static/js/2.feaeb6cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f59d8af1e1424e10ce2",
+    "url": "/Parh/static/js/main.b18688f5.chunk.js"
+  },
+  {
+    "revision": "dabc2c0d0afbc87c2198",
+    "url": "/Parh/static/js/runtime-main.c394a686.js"
+  },
+  {
+    "revision": "bca554bdf3d84b3b099435836ff794b6",
+    "url": "/Parh/static/media/NotoNaskhArabic-Regular.bca554bd.ttf"
+  }
+]);
